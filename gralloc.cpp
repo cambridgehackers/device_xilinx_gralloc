6bd9ce50b2afe2e1f4efcdc4f5ebc0ea06afb784
Jamey Hicks <jamey.hicks@qrclab.com>
2014-04-28 11:16:49 -0400

    android no longer supports 2 bytes per pixel formats

--- gralloc.cpp
+++ gralloc.cpp
@@ -226,11 +226,6 @@ static int gralloc_alloc(alloc_device_t* dev,
         case HAL_PIXEL_FORMAT_RGB_888:
             bpp = 3;
             break;
-        case HAL_PIXEL_FORMAT_RGB_565:
-        case HAL_PIXEL_FORMAT_RGBA_5551:
-        case HAL_PIXEL_FORMAT_RGBA_4444:
-            bpp = 2;
-            break;
         default:
             return -EINVAL;
     }
